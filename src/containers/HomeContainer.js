3e2d812d7561c3f8ac37f39b085ffc680865d57f
Conor Doyle <conorfrontend@gmail.com>
2017-12-19 23:51:45 +0000

    Add 'HomeContainer' and 'index.js' export helper for containers

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {  } from 'prop-types';
+
+
+class HomeContainer extends Component {
+  static propTypes = {
+
+  };
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div>
+        HomeContainer
+      </div>
+    );
+  }
+}
+
+export default HomeContainer;
