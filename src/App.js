a9d9e7bf0a01b403bb4b40ad31229276fe7f0d74
Conor Doyle <conorfrontend@gmail.com>
2017-12-19 23:53:11 +0000

    Add basic app.js, index.js, and routing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled, { injectGlobal } from 'styled-components';
+import { HomeContainer } from 'containers';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  html {
+    box-sizing: border-box;
   }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Wrapper>
+        <Switch>
+          <Route exact path='/' component={HomeContainer} />
+          <Route render={() => <p>Page Not Found!</p>} />
+        </Switch>
+      </Wrapper>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+// import * as reducers from 'ducks';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   combineReducers({...reducers}),
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  rootEl
+);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default; // eslint-disable-line global-require
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
+
 registerServiceWorker();
