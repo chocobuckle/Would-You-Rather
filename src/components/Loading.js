eff28b69f0fc6234bbf62f2ec9a4fd63583e4f41
Conor Doyle <conorfrontend@gmail.com>
2017-12-29 23:57:06 +0000

    Add 'Loading' component

---
+++ src/components/Loading.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { string, number } from 'prop-types';
+import styled from 'styled-components';
+
+class Loading extends Component {
+  static propTypes = {
+    text: string.isRequired,
+    speed: number.isRequired
+  }
+
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  }
+
+  state = {
+    text: this.props.text
+  }
+
+  componentDidMount() {
+    const { text, speed } = this.props;
+    const stopper = `${text}...`;
+    this.interval = window.setInterval(() => {
+      if (this.state.text === stopper) {
+        this.setState(() => {
+          return {
+            text
+          };
+        });
+      } else {
+        this.setState((prevState) => {
+          return {
+            text: `${prevState.text}.`
+          };
+        });
+      }
+    }, speed);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <Text>
+        {this.state.text}
+      </Text>
+    );
+  }
+}
+
+const Text = styled.p`
+  text-align: center;
+  margin: 0;
+`;
+
+export default Loading;
